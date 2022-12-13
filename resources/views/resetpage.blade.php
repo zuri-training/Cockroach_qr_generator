7e963ec920de9e5aa7183133a830d545e5187690
Belleza <aishat.omojole@gmail.com>
2022-12-13 10:01:44 +0100

    Reset page restyled

--- resources/views/resetpage.blade.php
+++ resources/views/resetpage.blade.php
@@ -10,27 +10,41 @@
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 
     <main class="main">
-
-        <div class="popup">
-            <form action="{{ route('password-reset', $token) }}" method="post">
-                @csrf
-                <div class="close-btn">&times;</div>
-                <div class="title">
-                    <h1>Let's get you a new password</h1>
-                    <p>Create a new password for your account</p>
-                </div>
-                <div class="pop-form">
-                    <label for="password">New Password</label>
-                    <input type="password" id="pass" name="password" required>
-                    <label for="pass_confirm">Confirm Password</label>
-                    <input type="password" id="pass_confirm" name="password_confirmation" required>
-                </div>
-                <div id="reset" class="reset-pass">
-                    <button type="submit" id="btn">Reset Password</button>
-                </div>
-
-            </form>
+    <!------------------forgot password popup------------------>
+    <div class="popup new-pass">
+        <div class="logo">
+            <h1>Let's get you a new password</h1>
+            <p>Create a new password for your account</p>
         </div>
+        <form action="{{ route('password-reset', $token) }}" method="post">
+                @csrf
+            <div class="dtype">
+                <label for="password">New Password</label>
+                <br>
+                <input type="password" id="password" name="password1"placeholder="8+ characters"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                    required>
+            </div>
+            <div class="dtype">
+
+                <label for="pass">Confirm Password</label>
+                <br>
+                <input type="password" id="pass" name="password1"  placeholder="8+ characters"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                    required>
+            </div>
+                <div class="reset">
+                    <button type="reset" class="login-btn" >Reset Password</button>
+                </div>
+
+        </form>        
+    </div>
+    <!-------------custom js link-------------------->
+    <script type="text/javascript" src="js/forgotpassword.js"></script>
+</main>
+
         {{-- 
 
         <div class="close-btn">&times;</div>
