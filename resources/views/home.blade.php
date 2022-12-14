b6f7b00e8fbf4e4a4ec8fd59d93e7691ba6a4e44
Belleza <aishat.omojole@gmail.com>
2022-12-14 15:40:48 +0100

    Removed search icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,30 +75,6 @@
                     <button id="sidebarToggleTop" class="btn  btn-link rounded-circle mr-3">
                         <i class="fa fa-bars" style="color:#5683F6;"></i>
                     </button>
-                    <ul class="navbar-nav ml-auto">
-
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-1 small"
-                                            placeholder="What do you want to look for?" aria-label="Search"
-                                            aria-describedby="basic-addon2" style="border-color: #5683F6;">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-                    </ul>
                 </nav>
                 <!-- Topbar -->
 
